6ab91b3c9bcd9b3316adc8dac3b3b0f8e9b80f2a
Nathan Krishnan <3166572+nathankrishnan@users.noreply.github.com>
2022-03-01 01:26:13 -0800

    Scaffold TaukPackageTest class

--- Tests/TaukTests/TaukTests.swift
+++ Tests/TaukTests/TaukTests.swift
@@ -1,11 +1,10 @@
 import XCTest
 @testable import Tauk
 
-final class TaukTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Tauk().text, "Hello, World!")
+final class TaukPackageTest: TaukXCTestCase {
+    
+    override func setUpWithError() throws {
+        taukSetUp(apiToken: "", projectId: "", appUnderTest: XCUIApplication.init())
     }
+    
 }
